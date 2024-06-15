62b9a8d10eafca52cad01238b48f407db322f5f9
Keller Jordan <kellerjordan1998@gmail.com>
2024-06-15 15:01:43 -0700

    .

---
+++ run.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0 python train.py &
+CUDA_VISIBLE_DEVICES=1 python train.py &
+CUDA_VISIBLE_DEVICES=2 python train.py &
+CUDA_VISIBLE_DEVICES=3 python train.py &
+CUDA_VISIBLE_DEVICES=4 python train.py &
+CUDA_VISIBLE_DEVICES=5 python train.py &
+CUDA_VISIBLE_DEVICES=6 python train.py &
+CUDA_VISIBLE_DEVICES=7 python train.py &
+wait
